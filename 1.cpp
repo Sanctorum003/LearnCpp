3cdf7961ebe257ea60344c0e510f771c0d573c94
Sanctorum003 <lushuchengsky@126.com>
2020-06-16 23:52:27 +0800

    update

--- 1.cpp
+++ 1.cpp
@@ -3,12 +3,9 @@ using namespace std;
 
 int main()
 {
-    int i = 0;
-    const int ci = i;
-
-    auto k = ci,&l = i;
-    auto &m = ci,*p = &ci;
-    auto *p2=& ci;
+    int a[] = {1,2,3,4,5,};
+    int *p = &a[2];
+    cout<<p[1]<<" "<<p[-1]<<endl;
 
     return 0;
 }
